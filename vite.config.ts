9f975a550f4b5672c63e8e603d112445441f645a
Nezketsu <greg.caseaux@gmail.com>
2025-06-03 20:13:41 +0200

    [fix] vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio/', // Add base URL for GitHub Pages
+  base: "./", // This makes all assets load relative to the current path
   plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
